3e92adb1fc0d7cad04ed5c894d83f202f318f7a8
ginstone <hjg719@139.com>
2022-10-21 10:47:13 +0800

    Initial commit

---
+++ src/main/java/com/github/gin/utils/Main.java
@@ -0,0 +1,13 @@
+package com.github.gin.utils;
+
+/**
+ * ${TODO}
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : ${DATE} ${HOUR}:${MINUTE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
